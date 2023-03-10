e6332ccb3273c87da30db669367dad08d6d4d66e
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-03-10 17:38:59 +0300

    RaporFormlarıEklendi

--- DiyetProgrami/WindowsFormsApp1/frmHaftalikAylikRapor.cs
+++ DiyetProgrami/WindowsFormsApp1/frmHaftalikAylikRapor.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class frmHaftalikAylikRapor : Form
     {
-        public Form1()
+        public frmHaftalikAylikRapor()
         {
             InitializeComponent();
-        }
-
-        
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- DiyetProgrami/WindowsFormsApp1/frmKiyasRaporu.cs
+++ DiyetProgrami/WindowsFormsApp1/frmKiyasRaporu.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class frmKiyasRaporu : Form
     {
-        public Form1()
+        public frmKiyasRaporu()
         {
             InitializeComponent();
-        }
-
-        
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
