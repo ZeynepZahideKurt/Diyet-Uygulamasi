03bc94a21a0c561cf39ac513aa9cc39c46322255
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-03-10 13:25:46 +0300

    Berna ekledi
    Selam Eklendi

--- DiyetProgrami/WindowsFormsApp1/Form1.cs
+++ DiyetProgrami/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,11 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        //Selam Ben Berna :)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
