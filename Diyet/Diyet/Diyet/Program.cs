07338589b2dce3a0e6d20217602429e2dad530a6
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-03-13 12:29:57 +0300

    deneme berna123

--- Diyet/Diyet/Diyet/Program.cs
+++ Diyet/Diyet/Diyet/Program.cs
@@ -8,6 +8,7 @@ namespace Diyet
 {
     internal static class Program
     {
+        //Deneme123
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
