d620c6cfdef68a75ccf86f162a2afbd59890ad17
Zahide Zeynep Kurt <zeynepugurcu97@gmail.com>
2023-03-13 03:09:42 +0300

    User class eklendi

---
+++ Diyet/Diyet/Diet_Model/Entity/User.cs
@@ -0,0 +1,32 @@
+﻿using Diet_Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*FileStream FsResim=new FileStream("ResimAdresi",FileMode.Open,FileAccess.Read);
+BinaryReader BrResim=new BinaryReader(FsResim);
+byte[] Resim=BrResim.ReadBytes((int)FsResim.Length);
+     brResim.Close();
+     fsResim.Close();
+ */
+namespace Diet_Model.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public int Size { get; set; }
+        public int kilo { get; set; }
+        public string Gender { get; set; }
+        public FileStream MyProperty { get; set; }
+
+        public UserType UserType { get; set; }
+        public List<string> Notes { get; set; }
+    }
+}
