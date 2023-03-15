8d162820157bcc1c1a70b5a7f57580e198919fe2
NadireCobanoglu <nadirecobanoglu@gmail.com>
2023-03-15 11:39:13 +0300

    category repository ve healtytipeklendi

---
+++ Diyet/Diyet/Diet_DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using Diet_Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diet_DAL.Repositories
+{
+    public class CategoryRepository
+    {
+
+        AppDbContext dbContext;
+        public CategoryRepository()
+        {
+            dbContext = new AppDbContext();
+        }
+
+        public List<Category> GetAllCategory()
+        {
+            return dbContext.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int categoryID)
+        {
+            return dbContext.Categories.Where(a => a.ID == categoryID).FirstOrDefault();
+        }
+
+        public bool Insert(Category category)
+        {
+            dbContext.Categories.Add(category);
+            return dbContext.SaveChanges() > 0;
+        }
+
+        public bool Update(Category category)
+        {
+            Category updateCategory = dbContext.Categories.Where(a => a.ID == category.ID).FirstOrDefault();
+            return dbContext.SaveChanges() > 0;
+        }
+
+        public bool Delete(Category category)
+        {
+            Category deleteCategory = dbContext.Categories.Where(a => a.ID == category.ID).FirstOrDefault();
+            dbContext.Categories.Remove(deleteCategory);
+            return dbContext.SaveChanges() > 0;
+        }
+
+
+    }
+}
---
+++ Diyet/Diyet/Diet_DAL/Repositories/HealthyTipRepository.cs
@@ -0,0 +1,51 @@
+﻿using Diet_Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diet_DAL.Repositories
+{
+    public class HealthyTipRepository
+    {
+
+        AppDbContext dbContext;
+        public HealthyTipRepository()
+        {
+            dbContext = new AppDbContext();
+        }
+            public List<HealthyTip> GetAllHealthyTip()
+            {
+                return dbContext.HealthyTips.ToList();
+            }
+
+            public HealthyTip GetHealthyTipById(int healthytipID)
+            {
+                return dbContext.HealthyTips.Where(a => a.ID == healthytipID).FirstOrDefault();
+            }
+
+            public bool Insert(HealthyTip healthyTip)
+            {
+                dbContext.HealthyTips.Add(healthyTip);
+                return dbContext.SaveChanges() > 0;
+            }
+
+            public bool Update(HealthyTip healthyTip)
+            {
+                HealthyTip updateHealthyTip = dbContext.HealthyTips.Where(a => a.ID == healthyTip.ID).FirstOrDefault();
+                return dbContext.SaveChanges() > 0;
+            }
+
+            public bool Delete(HealthyTip healthyTip)
+            {
+                HealthyTip deleteHealthyTip = dbContext.HealthyTips.Where(a => a.ID == healthyTip.ID).FirstOrDefault();
+                dbContext.HealthyTips.Remove(healthyTip);
+                return dbContext.SaveChanges() > 0;
+            }
+
+
+
+
+        }
+}
