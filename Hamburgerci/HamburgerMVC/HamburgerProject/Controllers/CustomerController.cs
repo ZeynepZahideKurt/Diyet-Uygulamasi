901258d55f4c18c506061ecf7597165319393659
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-04-30 17:48:26 +0300

    OrdetDetails eklenmeden onceki versiyon

--- Hamburgerci/HamburgerMVC/HamburgerProject/Controllers/CustomerController.cs
+++ Hamburgerci/HamburgerMVC/HamburgerProject/Controllers/CustomerController.cs
@@ -7,6 +7,7 @@ using HamburgerProject.Repositories.Abstract;
 using HamburgerProject.Repositories.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
 
 namespace HamburgerProject.Controllers
 {
@@ -16,7 +17,7 @@ namespace HamburgerProject.Controllers
          Create action OK Create VİEW NOT_OK (Authentication bağlantılı userid taşıma yapılmalı)
          Update action NOT_OK Update VİEW NOT_OK
          Delete action NOT_OK Delete VİEW NOT_OK
-         List action NOT_OK ListVİEW NOT_OK
+         List action NOT_OK ListVİEW NOT_OK (ZEHRA)
          Authentication işlemleri
          Seed'e 2 3 adet veri ekle(Menu Extra)
         */
@@ -82,14 +83,14 @@ namespace HamburgerProject.Controllers
        
         public async Task<IActionResult> OrderAdd() 
         {
-           
+
             var menuList = menuGenericRepository.GetAll().ToList();
             var extraList = extraGenericRepository.GetAll().ToList();
             int id = 5;
             var user = await userManager.FindByIdAsync(id.ToString());
             Order order = new Order();
             order.AppUser = user;
-            bool result=orderRepository.Add(order);
+            bool result = orderRepository.Add(order);
             if (result)
             {
                 AddOrderVM orderList = new AddOrderVM();
@@ -99,34 +100,54 @@ namespace HamburgerProject.Controllers
                 orderList.UserId = order.AppUser.Id;
                 return View(orderList);
             }
+
             return View();
             
         }
         [HttpPost]
-        public async Task< IActionResult> OrderAdd(AddOrderVM orderList)
+        public async Task<IActionResult> OrderAdd(AddOrderVM orderList)
         {
+            
 
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
             Order order = orderRepository.GetById(orderList.OrderId);
             var menu = menuGenericRepository.GetById(orderList.MenuId);
             var extra = extraGenericRepository.GetById(orderList.ExtraId);
             var user = await userManager.FindByIdAsync(orderList.UserId.ToString());
-
             order.Menus.Add(menu);
             order.Extras.Add(extra);
-            order.AppUser= user;
+            order.AppUser = user;
             order.CreatedTime = orderList.CreateTime;
             orderRepository.Calculate(order);
 
+            orderList.AdminMenus= menuGenericRepository.GetAll().ToList();
+            orderList.AdminExtras= extraGenericRepository.GetAll().ToList();
+
+            orderList.CustomerMenus.Add(menu);
+            orderList.CustomerExtras.Add(extra);
+            orderList.OrderId = order.Id;
+            //orderList.CustomerMenus.Add(menu);
+            //orderList.CustomerExtras.Add(extra);
+            //orderList.Totalprice = order.TotalPrice;
+            //var menuList = menuGenericRepository.GetAll().ToList();
+            //var extraList = extraGenericRepository.GetAll().ToList();
+            //orderList.AdminExtras = extraList;
+            //orderList.AdminMenus = menuList;
             bool result = orderRepository.Update(order);
 
             if (result)
             {
                 TempData["Bildiri"] = "İşlem Başarılı";
-                return RedirectToAction("Index");
+                return View(orderList);
             }
+
+
             ViewBag.Bildiri = "İşlem Başarılı olamadı";
             return View(orderList);
-           
+
         }
     }
 }
